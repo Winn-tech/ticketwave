57f42e48f8840451aa914e2cc717e7aa2b51efb8
winn-tech <olugodwin99@gmail.com>
2024-10-26 17:12:38 +0100

    updated the verify page

--- src/Pages/eventOwnerAttendance.jsx
+++ src/Pages/eventOwnerAttendance.jsx
@@ -4,7 +4,9 @@ import CreatorsSidebar from '../Components/event-creators/creatorsSidebar';
 import {Bars} from 'react-loader-spinner'
 const EventOwnerAttendance = () => {
     const [UniquePin, setUniquecode] = useState('')
+    const [eventName, setEventName] = useState('')
     const [loading, setLoading] = useState(false)
+  
   return (
     <> 
        <CreatorsNavbar/>
@@ -12,10 +14,13 @@ const EventOwnerAttendance = () => {
        <CreatorsSidebar/>
           <div className="sub-container">
             <div className="verification-container">
-                <p>Input the attendee's Unique ticket code</p>
+                <h3>VALIDATE TICKETS</h3>
                 <form action="">
-                    <input type="text" placeholder='eg: therav3099bbrt' value={UniquePin} onChange={(e)=>setUniquecode(e.target.value)}/>
-                    <button>{loading?  <Bars color="white" height="20" />: "Verify" }</button>
+                   <label htmlFor="eventName">Event Name</label> 
+                   <input type="text"  placeholder='Ticketwave Pool party' id='eventName' value={eventName} onChange={(e)=>setEventName(e.target.value)}/>
+                    <label htmlFor="eventId">Ticket ID</label>
+                    <input type="text" placeholder='eg: therav3099bbrt' value={UniquePin} id='eventId' onChange={(e)=>setUniquecode(e.target.value)}/>
+                    <button>{loading?  <Bars color="white" height="20" />: "Validate Ticket" }</button>
                 </form>
             
             </div>
